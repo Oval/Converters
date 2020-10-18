2ec24182d9570b89e87454d4ec6337874b9acc0e
Oval <42103580+Oval@users.noreply.github.com>
2020-10-18 11:37:13 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,17 +1,15 @@
-import Converters.EpochConverter;
-import Converters.RomanConverter;
+import Converters.*;
 
 public class main {
 
 	/*
 	 * Testing purposes only. Example of the use to call.
 	 */
-	
 	public static void main(String[] args) {
 		RomanConverter.INSTANCE.ArabicToRoman(55);
-		
 		//Example of format that needs to be given
 		EpochConverter.INSTANCE.DateToEpoch("2014/18/08 18:52:21");
+		//Example
+		TextCapitalizationConverter.INSTANCE.TextCapitalizationChanger("random", "This is just an example!");
 	}
-
 }
