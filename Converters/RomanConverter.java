91c48dc4eae8f961183883dd1a7a34bcfde1e3ba
Oval <42103580+Oval@users.noreply.github.com>
2021-10-01 23:12:44 +0200

    Update RomanConverter.java
    More work

--- Converters/RomanConverter.java
+++ Converters/RomanConverter.java
@@ -1,28 +1,14 @@
 package Converters;
 
+import java.util.Collections;
+
 public enum RomanConverter {
 	INSTANCE;
 
 	public final String ToRoman(int number) {
-		TreeMap<Integer, String> m = new TreeMap<Integer, String>();
-		m.put(1000, "M");
-		m.put(900, "CM");
-		m.put(500, "D");
-		m.put(400, "CD");
-		m.put(100, "C");
-		m.put(90, "XC");
-		m.put(50, "L");
-		m.put(40, "XL");
-		m.put(10, "X");
-		m.put(9, "IX");
-		m.put(5, "V");
-		m.put(4, "IV");
-		m.put(1, "I");
-
-		int l = m.floorKey(number);
-		if (number == l) {
-			return m.get(number);
-		}
-		return m.get(l) + ToRoman(number - l);
+		return String.join("", Collections.nCopies(number, "I")).replace("IIIII", "V").replace("IIII", "IV")
+				.replace("VV", "X").replace("VIV", "IX").replace("XXXXX", "L").replace("XXXX", "XL").replace("LL", "C")
+				.replace("LXL", "XC").replace("CCCCC", "D").replace("CCCC", "CD").replace("DD", "M")
+				.replace("DCD", "CM");
 	}
 }
